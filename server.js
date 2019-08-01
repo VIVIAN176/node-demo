69dc01e09260a5edb803adc43c68fe3b951df13a
auntzhang <xjlady@126.com>
2019-08-01 16:37:19 +0800

    a

--- server.js
+++ server.js
@@ -19,39 +19,47 @@ var server = http.createServer(function (request, response) {
 
     /******** 从这里开始看，上面不要看 ************/
 
-    console.log('HTTP路径为\n' + path)
-    if (path == '/') {
+    console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
+
+    if (path === '/') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write('<!DOCTYPE html>
-            < html >
-            <head>
-                <link rel="stylesheet" href="/style">
-                    <link rel="stylesheet" href="/js">
-                </head>
-                    <body>
-                        <h1>hello myfist node</h1>
-                    </body>
-            </html>')
-                        response.end()
-    } else if（path == 'style'）{
-                    response.statusCode = 200
-                    response.write('body{backgound - color: #ddd;}h1{color: red}')
-    response.setHeader('Content-Type', 'text/css;charset=utf-8')
-                    response.end()
-    } else if (path == 'script') {
-                    response.statusCode = 200
-                    response.write('alert("这是js执行的")')
-    response.setHeader('Content-Type', 'text/js;charset=utf-8')
-                    response.end()
-                }
-    else {
-                    response.statusCode = 404
+        response.write(`
+      <!DOCTYPE html>
+      <head>
+        <link rel="stylesheet" href="/style"/>
+      </head>
+
+      <body>
+         <h1>你好</h1>
+         <script src="/main"></script>
+      </body>
+    `)
         response.end()
-                            }
-                        
-                            /******** 代码结束，下面不要看 ************/
-                        
-                        
-                            server.listen(port)
-    console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+    } else if (path === '/style') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/css;charset=utf-8')
+        response.write(`
+        body{backgound - color: #ddd;}
+        h1{color: red}
+    `)
+        response.end()
+    } else if (path === '/main') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+        response.write(`
+        alert("这是js执行的")
+    `)
+        response.end()
+    } else {
+        response.statusCode = 404
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write('呜呜呜')
+        response.end()
+    }
+
+    /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
