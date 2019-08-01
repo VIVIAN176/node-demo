0de5459b9b2094fe6a14088c30f2c0ec5d03fbed
auntzhang <xjlady@126.com>
2019-08-01 13:11:01 +0800

    11

--- server.js
+++ server.js
@@ -21,17 +21,17 @@ var server = http.createServer(function (request, response) {
 
     console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-    if (path === '/') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write('哈哈哈')
-        response.end()
-    } else {
-        response.statusCode = 404
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write('呜呜呜')
-        response.end()
-    }
+    // if (path === '/') {
+    //     response.statusCode = 200
+    //     response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    //     response.write('哈哈哈')
+    //     response.end()
+    // } else {
+    //     response.statusCode = 404
+    //     response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    //     response.write('呜呜呜')
+    //     response.end()
+    // }
 
     /******** 代码结束，下面不要看 ************/
 })
